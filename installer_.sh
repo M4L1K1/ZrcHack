e855d1b89292f2673df26b8979d02b6e5902012f
xscrprog <42752384+xscrprog@users.noreply.github.com>
2019-12-30 11:50:13 +0700

    Update installer_.sh

--- installer_.sh
+++ installer_.sh
@@ -7,10 +7,7 @@ time apt install tput rsync aapt neofetch toilet ncurses-utils tsu openssl-tool 
 
 tsu -c 'find . -iname '*scrz*' -exec rm -rf {} \;'
 
-[[ "$(uname -m)" =~ 'aarch64' ]] || {
-    printf "Your device's architecture isn't officially supported yet."
-    exit 1
-}
+
 
 tsu -c 'wget https://raw.githubusercontent.com/xscrprog/ZrcHack/master/allarchi/scrz_cli -O ./scrz_cli'
 tsu -c 'chmod a+x scrz_cli'

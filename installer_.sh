e16c73156562c7723100f385e789c5c3b9101f42
xscrprog <42752384+xscrprog@users.noreply.github.com>
2020-01-05 22:45:33 +0700

    Update installer

--- installer_.sh
+++ installer_.sh
@@ -1,12 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 apt-get update
 apt-get --assume-yes upgrade 
-apt-get --assume-yes install wget
 su -c 'setenforce 0'
+apt-get --assume-yes install tsu
+apt-get --assume-yes install wget
+apt-get --assume-yes install grep
+apt-get --assume-yes install bc
+apt-get --assume-yes install toilet
+apt-get --assume-yes install tput rsync aapt neofetch ncurses-utils openssl-tool ruby lolcat
 
-apt-get --assume-yes install tput rsync aapt neofetch toilet ncurses-utils tsu openssl-tool ruby lolcat bc
 tsu -c 'find . -iname '*scrz*' -exec rm -rf {} \;'
-
 tsu -c 'wget https://raw.githubusercontent.com/xscrprog/ZrcHack/master/allarchi/scrz_cli -O ./scrz_cli'
 tsu -c 'chmod a+x scrz_cli'
 tsu -c ./scrz_cli
